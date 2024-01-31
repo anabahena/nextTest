a1f3937096aa275e7d692b81ec683d3d65813ef2
anabahena <ana.gonzalezb@ab-inbev.com>
2024-01-31 12:01:34 -0600

    feat: update dashboard data and UI

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -24,7 +24,7 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
-
+    console.log('data', data)
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
